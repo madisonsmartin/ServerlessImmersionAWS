761eb3f7e0ba2af8d3c5cecd47f2f212255d3739
mdaisonsmartin <madison.53.martin@gmail.com>
2019-09-24 13:41:53 +0000

    modified congnito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_v7zgPGwxh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2gnaf9196nqahjfaa7tjdnjvfti', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2gnaf9196nqahjfaa7tjdnjvft', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
